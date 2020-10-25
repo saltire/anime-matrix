6ef76d74562d53e7ef2aba7de05febfbf107b056
saltire sable <saltiresable@gmail.com>
2020-10-25 16:14:51 -0400

    Store rows as (index, width) tuple

--- src/main.rs
+++ src/main.rs
@@ -30,65 +30,65 @@ const PANE2_HEADER: [u8; 7] = [0x5e, 0xc0, 0x02, 0x74, 0x02, 0x73, 0x02];
 const FLUSH: [u8; 3] = [0x5e, 0xc0, 0x03];
 
 // 55 rows, 1215 total
-const PANE1_ROW_INDICES: [[usize; 2]; 21] = [
-  [8, 39], // 1 short on right side
-  [41, 73],
-  [76, 108],
-  [109, 141],
-  [144, 176],
-  [177, 209],
-  [211, 243],
-  [244, 275],
-  [277, 308],
-  [309, 339],
-  [341, 371],
-  [372, 401],
-  [403, 432],
-  [433, 461],
-  [463, 491],
-  [492, 519],
-  [521, 548],
-  [549, 575],
-  [577, 603],
-  [604, 629],
-  [631, 633], // first 3 pixels of row
+const PANE1_ROWS: [(usize, usize); 21] = [
+  (8, 32), // 1 short on right side
+  (41, 33),
+  (76, 33),
+  (109, 33),
+  (144, 33),
+  (177, 33),
+  (211, 33),
+  (244, 32),
+  (277, 32),
+  (309, 31),
+  (341, 31),
+  (372, 30),
+  (403, 30),
+  (433, 29),
+  (463, 29),
+  (492, 28),
+  (521, 28),
+  (549, 27),
+  (577, 27),
+  (604, 26),
+  (631, 3), // first 3 pixels of row
 ];
-const PANE2_ROW_INDICES: [[usize; 2]; 35] = [
-  [7, 29], // starts at 4th pixel of last row of pane 1
-  [30, 54],
-  [56, 80],
-  [81, 104],
-  [106, 129],
-  [130, 152],
-  [154, 176],
-  [177, 198],
-  [200, 221],
-  [222, 242],
-  [244, 264],
-  [265, 284],
-  [286, 305],
-  [306, 324],
-  [326, 344],
-  [345, 362],
-  [364, 381],
-  [382, 398],
-  [400, 416],
-  [417, 432],
-  [434, 449],
-  [450, 464],
-  [466, 480],
-  [481, 494],
-  [496, 509],
-  [510, 522],
-  [524, 536],
-  [537, 548],
-  [550, 561],
-  [562, 572],
-  [574, 584],
-  [585, 594],
-  [596, 605],
-  [606, 614],
-  [616, 624],
+const PANE2_ROWS: [(usize, usize); 35] = [
+  (7, 23), // starts at 4th pixel of last row of pane 1
+  (30, 25),
+  (56, 25),
+  (81, 24),
+  (106, 24),
+  (130, 23),
+  (154, 23),
+  (177, 22),
+  (200, 22),
+  (222, 21),
+  (244, 21),
+  (265, 20),
+  (286, 20),
+  (306, 19),
+  (326, 19),
+  (345, 18),
+  (364, 18),
+  (382, 17),
+  (400, 17),
+  (417, 16),
+  (434, 16),
+  (450, 15),
+  (466, 15),
+  (481, 14),
+  (496, 14),
+  (510, 13),
+  (524, 13),
+  (537, 12),
+  (550, 12),
+  (562, 11),
+  (574, 11),
+  (585, 10),
+  (596, 10),
+  (606, 9),
+  (616, 9),
 ];
 const PANE2_Y_OFFSET: usize = 20;
 const PANE2_X_OFFSET: usize = 3;
@@ -101,7 +101,6 @@ fn send(buf: &[u8], device_handle: &DeviceHandle<GlobalContext>, timeout: &Durat
   // println!("{:02X?}", buffer);
 
   match device_handle.write_control(REQUEST_TYPE, REQUEST, VALUE, 0, &buffer, *timeout) {
-    // Ok(size) => println!("{} written", size),
     Ok(_) => {},
     Err(e) => println!("Error: {}", e)
   }
@@ -134,17 +133,12 @@ fn usb() {
       }
 
       let mut widths = vec![];
-      for indices in PANE1_ROW_INDICES.iter() {
-        widths.push(indices[1] - indices[0] + 1);
+      for (_index, width) in PANE1_ROWS.iter() {
+        widths.push(*width);
       }
-      for (r, indices) in PANE2_ROW_INDICES.iter().enumerate() {
-        if r == 0 {
-          let p1_last_width = widths.pop().unwrap();
-          widths.push(p1_last_width + indices[1] - indices[0] + 1);
-        }
-        else {
-          widths.push(indices[1] - indices[0] + 1);
-        }
+      for (r, (_index, width)) in PANE2_ROWS.iter().enumerate() {
+        let last_width = if r == 0 { widths.pop().unwrap() } else { 0 };
+        widths.push(last_width + width);
       }
       let mut pixels = vec![];
       for width in widths {
@@ -158,9 +152,9 @@ fn usb() {
       for x in 0..7 {
         pane1[x] = PANE1_HEADER[x];
       }
-      for (r, indices) in PANE1_ROW_INDICES.iter().enumerate() {
-        for (i, x) in (indices[0]..(indices[1] + 1)).enumerate() {
-          pane1[x] = pixels[r][i];
+      for (r, (index, width)) in PANE1_ROWS.iter().enumerate() {
+        for x in 0..*width {
+          pane1[index + x] = pixels[r][x];
         }
       }
 
@@ -168,10 +162,10 @@ fn usb() {
       for x in 0..7 {
         pane2[x] = PANE2_HEADER[x];
       }
-      for (r, indices) in PANE2_ROW_INDICES.iter().enumerate() {
+      for (r, (index, width)) in PANE2_ROWS.iter().enumerate() {
         let x_offset = if r == 0 { PANE2_X_OFFSET } else { 0 };
-        for (i, x) in (indices[0]..(indices[1] + 1)).enumerate() {
-          pane2[x] = pixels[r + PANE2_Y_OFFSET][i + x_offset];
+        for x in 0..*width {
+          pane2[index + x] = pixels[r + PANE2_Y_OFFSET][x + x_offset];
         }
       }
 
