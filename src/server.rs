df663d0efad0e4c201c8e47e846f2b4301897bf6
saltire sable <saltiresable@gmail.com>
2020-11-10 00:30:11 -0500

    Post grid values to server

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,21 @@
+use serde::Deserialize;
+use rocket_contrib::json::Json;
 use rocket_contrib::serve::StaticFiles;
+
+
+#[derive(Deserialize)]
+struct Data {
+  values: Vec<u8>,
+}
+
+#[post("/post", format = "json", data = "<data>")]
+fn post(data: Json<Data>) -> () {
+  println!("{:?}", data.values);
+}
 
 pub fn start() {
   rocket::ignite()
     .mount("/", StaticFiles::from("web"))
+    .mount("/", routes![post])
     .launch();
 }
--- web/web.js
+++ web/web.js
@@ -1,7 +1,9 @@
-const ns = 'http://www.w3.org/2000/svg';
 const grid = document.querySelector('#grid');
 const slide = document.querySelector('#slide');
 const current = document.querySelector('#current-color');
+const button = document.querySelector('button');
+
+const ns = 'http://www.w3.org/2000/svg';
 
 const width = 10;
 const height = 10;
@@ -11,8 +13,9 @@ const canvasheight = ((height / 2) + 0.5) * size;
 
 grid.setAttribute('viewBox', `0 0 ${canvaswidth} ${canvasheight}`);
 
-let color = 'white';
+let color = '#ffffff';
 let painting = null;
+const map = {};
 
 const drawDiamond = (x, y) => {
   const poly = document.createElementNS(ns, 'polygon');
@@ -27,20 +30,12 @@ const drawDiamond = (x, y) => {
     ]
     .map(point => point.map(v => v * size).join(','))
     .join(' '));
-  poly.setAttribute('fill', 'black');
+  poly.setAttribute('fill', '#000000');
   grid.appendChild(poly);
 
   poly.onmousedown = () => {
     poly.setAttribute('fill', color);
     painting = color;
-    // if (poly.getAttribute('fill') === 'blue') {
-    //   poly.setAttribute('fill', 'red');
-    //   painting = 'red';
-    // }
-    // else {
-    //   poly.setAttribute('fill', 'blue');
-    //   painting = 'blue';
-    // }
   };
 
   poly.onmouseenter = () => {
@@ -48,10 +43,12 @@ const drawDiamond = (x, y) => {
       poly.setAttribute('fill', painting);
     }
   };
+
+  map[`${x},${y}`] = poly;
 };
 
-for (x = 0; x < 10; x++) {
-  for (y = 0; y < 10; y++) {
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
     drawDiamond(x, y);
   }
 }
@@ -66,6 +63,22 @@ grid.onmouseleave = () => {
 slide.onmousedown = (e) => {
   const rect = e.target.getBoundingClientRect();
   const value = Math.round((e.x - rect.x) / rect.width * 255);
-  color = `rgb(${value}, ${value}, ${value})`;
+  const hex = value.toString(16);
+  color = `#${hex}${hex}${hex}`;
   current.setAttribute('fill', color);
 }
+
+button.onclick = () => {
+  const values = [];
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      values.push(parseInt(map[`${x},${y}`].getAttribute('fill').slice(1, 3), 16));
+    }
+  }
+
+  fetch('/post', {
+    method: 'post',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ values }),
+  });
+};
